2428d849c5e86010028ce6001f98fe82c53dcd07
Wasim Akaram <55456khanahmad@gmail.com>
2022-12-04 14:46:25 +0530

    login completed

--- index.js
+++ index.js
@@ -3,19 +3,21 @@
 let n=13
 let count=0
 for(let i=1;i<=13;i++){
-  if(n%i==0){count++}
+  if(n%i==0){
+count++;
 }
-if(count==1){console.log("it is Prime number")}
-else {console.log("it is not a Prime number")}
+}
+if(count==2){
+console.log("it is Prime number")
+}
+else {
+console.log("it is not a Prime number")
+}
 
 //Palindrome
-
-let str="racecar"
-let t=false
-for(let i=0;i<=str.length;i++){
-  if(str[i]==str[str.length-1-i]){
-    t=true
-  }
+let str = "racecar";
+let bag = "";
+for(let i = str.length-1; i>=0; i--){
+  bag =bag+str[i];
 }
-if(t==true){console.log("its Palindrome")}
-else{console.log("its not a Palindrome")}
+(bag ==str)?console.log("It's a Palindrome."):console.log("Not a Palindrome");
